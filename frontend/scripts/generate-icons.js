fbbc99a643e716fcfac5974e8556da9607e87730
shashanknagariya <nagariya.shashank7@gmail.com>
2025-03-24 17:32:32 +0530

    minor fixes

--- frontend/scripts/generate-icons.js
+++ frontend/scripts/generate-icons.js
@@ -1,6 +1,10 @@
-const sharp = require('sharp');
-const fs = require('fs');
-const path = require('path');
+import sharp from 'sharp';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const ICONS_DIR = path.join(__dirname, '../public/icons');
 const SOURCE_ICON = path.join(__dirname, '../src/assets/logo.png');
